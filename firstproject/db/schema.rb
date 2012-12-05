c230a71b2dc4f82fa431b4510353c8e515f3a4e1
SOA2012-101078515 <changcheng.tu@gmail.com>
2012-12-05 19:44:01 +0800

    2

--- firstproject/db/schema.rb
+++ firstproject/db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121113143248) do
+ActiveRecord::Schema.define(:version => 20121127130921) do
+
+  create_table "events", :force => true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.boolean  "is_public"
+    t.integer  "capacity"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "requests", :force => true do |t|
     t.string   "name"
